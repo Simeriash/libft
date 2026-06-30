c9af3f570b938a8624e245129778397c615a59f4
julauren <249057325+Simeriash@users.noreply.github.com>
2026-06-30 17:30:36 +0200

    fix strcmp & strncmp

--- src/ft_strcmp.c
+++ src/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 10:14:49 by julauren          #+#    #+#             */
-/*   Updated: 2026/06/10 12:12:39 by julauren         ###   ########.fr       */
+/*   Updated: 2026/06/30 17:29:56 by julauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strcmp(const char *s1, const char *s2)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	while (s1[i] && s2[i] && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 13:52:54 by julauren          #+#    #+#             */
-/*   Updated: 2026/06/10 12:12:23 by julauren         ###   ########.fr       */
+/*   Updated: 2026/06/30 17:29:23 by julauren         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+	while (s1[i] != '\0' && s2[i] != '\0'  && s1[i] == s2[i] && i < n)
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
